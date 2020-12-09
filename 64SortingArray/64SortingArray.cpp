1fd9a23c60467ae71af395f63f754a7ac44ef8fc
Bowen <yuan.bowen@yahoo.com>
2020-12-10 09:21:27 +1100

    Array
    6.1Array

---
+++ 64SortingArray/64SortingArray.cpp
@@ -0,0 +1,53 @@
+// 64SortingArray.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <algorithm> // for std::swap, use <utility> instead if C++11
+#include <iostream>
+#include <iterator>
+
+int main()
+{
+	int array[]{ 30, 50, 20, 10, 40 };
+	constexpr int length{ static_cast<int>(std::size(array)) };
+
+	// Step through each element of the array
+	// (except the last one, which will already be sorted by the time we get there)
+	for (int startIndex{ 0 }; startIndex < length - 1; ++startIndex)
+	{
+		// smallestIndex is the index of the smallest element we’ve encountered this iteration
+		// Start by assuming the smallest element is the first element of this iteration
+		int smallestIndex{ startIndex };
+
+		// Then look for a smaller element in the rest of the array
+		for (int currentIndex{ startIndex + 1 }; currentIndex < length; ++currentIndex)
+		{
+			// If we've found an element that is smaller than our previously found smallest
+			if (array[currentIndex] < array[smallestIndex])
+				// then keep track of it
+				smallestIndex = currentIndex;
+		}
+
+		// smallestIndex is now the smallest element in the remaining array
+				// swap our start element with our smallest element (this sorts it into the correct place)
+		std::swap(array[startIndex], array[smallestIndex]);
+	}
+
+	// Now that the whole array is sorted, print our sorted array as proof it works
+	for (int index{ 0 }; index < length; ++index)
+		std::cout << array[index] << ' ';
+
+	std::cout << '\n';
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
